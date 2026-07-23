bc566c771a430e7dfd216d23641b8b431fe1abd5
devishraq <fofbp926942@gmail.com>
2026-07-24 02:54:41 +0600

    Remove unused load_pair import from sae_loader in __init__.py

--- sae_canonical_units/__init__.py
+++ sae_canonical_units/__init__.py
@@ -1,5 +1,5 @@
 from .controls import random_decoder_control, same_width_control
 from .meta_sae import TopKSAE, train_meta_sae
 from .model_utils import get_activations, load_model
-from .sae_loader import load_pair, load_sae
+from .sae_loader import load_sae
 from .stitching import stitching_novel_fraction
