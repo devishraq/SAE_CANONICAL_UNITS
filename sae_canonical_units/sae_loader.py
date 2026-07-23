3da82ea2923ffa2dae7e1124d5d567ed5d2e9e43
devishraq <fofbp926942@gmail.com>
2026-07-24 02:54:35 +0600

    Add load_gpt2_small_saes function to load GPT-2 small sae models

--- sae_canonical_units/sae_loader.py
+++ sae_canonical_units/sae_loader.py
@@ -10,6 +10,13 @@ def load_sae(release, sae_id):
         p.requires_grad = False
     return sae
 
+def load_gpt2_small_saes():
+    small_768 = load_sae("gpt2-small-res-jb", "blocks.8.hook_resid_pre_768")
+    small_4k = load_sae("gpt2-small-res-jb", "blocks.8.hook_resid_pre_4096")
+    large_16k = load_sae("gpt2-small-res-jb", "blocks.8.hook_resid_pre_16384")
+    large_32k = load_sae("gpt2-small-res-jb", "blocks.8.hook_resid_pre_32768")
+    return small_768, small_4k, large_16k, large_32k
+
 def load_gemma_2b_saes():
     small = load_sae("gemma-scope-2b-pt-res", "layer_10/width_16k/canonical")
     large = load_sae("gemma-scope-2b-pt-res", "layer_10/width_65k/canonical")
